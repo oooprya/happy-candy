f582b34ef9e55007924b34231d1617f53d9a3362
oooprya <32761598+oooprya@users.noreply.github.com>
2017-10-19 10:55:35 +0300

    random questions

--- happy-candy/js/main.js
+++ happy-candy/js/main.js
@@ -5,52 +5,9 @@ var winTitle = document.getElementById('win-result');
 var rBlock = document.getElementById('questBlock');
 var summ = 0;
 var randSumm = [];
+var happyArray = [];
 function start(){
-nextQuestion();	
-winTitle.innerHTML = '';
-}
-function nextQuestion(){
-	//Выбираем из массива сгенерированные 3 в числа
-	for (var i = 0; i < randSumm.length; i++) {
-	namberQuest = i;
-	var strHtml = '<h2>'+happyArray[namberQuest]["question"][0]+'</h2><div class="wrap-radio" >';
-	for (var i = 0; i < happyArray[namberQuest]["answer"].length; i++) {
-		j = happyArray[namberQuest]["answer"][i];
-		strHtml += '<div class="fl-l col-sm-6"> <input type="radio" value="'+i+'" onclick="check('+i+');" name="r" id="sub_'+i+'" /><label for="sub_'+i+'">'+j+'</label></div>';	
-	}
-	rBlock.innerHTML = strHtml +'</div>';
-	}
-}
-function nextQuest() {
-if (!endQuest()) {
-		winTitle.innerHTML = '<div class="center-result"><div class="t-cell"><div class="win-bg"><h1>Тест пройден</h1> <div>Вы набрали <h2>'+summ+'</h2>баллов </div><div class="btnGreen" onclick="start()">Заново</div></div></div></div>';
-		return rBlock.innerHTML =  '';
-	}
-namberQuest++;
-nextQuestion();
-winTitle.innerHTML = '';
-}
-function check(i) {
-	console.log(i, happyArray[namberQuest]["answer"][i]);	
-	if (i == happyArray[namberQuest]["answerRight"][0]){
-		winTitle.innerHTML = '<div class="center-result"><div class="t-cell"><div class="win-bg"><h1>Правильно</h1> <br><div class="btnGreen" id="btnNext" onclick="nextQuest();" >Далее</div></div></div></div>';
-		summ += 5;
-	}
-	else{
-		winTitle.innerHTML = '<div class="center-result"><div class="t-cell"><div class="win-bg"><h1>Не правилно</h1> <br><div class="btnGreen" id="btnNext" onclick="nextQuest();" >Далее</div></div></div></div>';
-	}
-	var radioDis = document.getElementsByName("r");
-	for (var i = 0; i< radioDis.length;  i++){
-    radioDis[i].disabled = true;
-	}
-}
-function endQuest(){
-	if (happyArray.length-1 == namberQuest) {
-		return false;	
-	} return true;
-	console.log(happyArray.length-1, namberQuest);
-}
-var happyArray = [
+happyArray = [
 	    {
 		'question' : ['Сколько пальцев на руках у человека?'],
 	  	'answer' : ['5 пальцев', '10 пальцев', '20 пальцев' , '15 пальцев'], 
@@ -77,10 +34,53 @@ var happyArray = [
 	  	'answerRight' : ['2']	
 	  	}
 ];
-function randomInteger() {
+namberQuest = 0;
+nextQuestion();	
+winTitle.innerHTML = '';
+}
+function nextQuestion(){
+	var rand = Math.floor((Math.random() * happyArray.length));
+	namberQuest = rand;
+	console.log('номер вопроса: ' +namberQuest);
+	var strHtml = '<h2>'+happyArray[namberQuest]["question"][0]+'</h2><div class="wrap-radio" >';
+	for (var i = 0; i < happyArray[namberQuest]["answer"].length; i++) {
+		j = happyArray[namberQuest]["answer"][i];
+		strHtml += '<div class="fl-l col-sm-6"> <input type="radio" value="'+i+'" onclick="check('+i+');" name="r" id="sub_'+i+'" /><label for="sub_'+i+'">'+j+'</label></div>';	
+	}
+	rBlock.innerHTML = strHtml +'</div>';
+
+}
+function nextQuest() {
+if (happyArray.length > 0) {
+		happyArray.splice(namberQuest, 1);
+		console.log('удален индекс №: ' +namberQuest);
+	}	
+if (happyArray.length == 0) {
+		winTitle.innerHTML = '<div class="center-result"><div class="t-cell"><div class="win-bg"><h1>Тест пройден</h1> <div>Вы набрали <h2>'+summ+'</h2>баллов </div><div class="btnGreen" onclick="start()">Заново</div></div></div></div>';
+		return rBlock.innerHTML =  '';
+	}
+	nextQuestion();
+	winTitle.innerHTML = '';
+}
+function check(i) {
+	//console.log(i, happyArray[namberQuest]["answer"][i]);
+
+	if (i == happyArray[namberQuest]["answerRight"][0]){
+		winTitle.innerHTML = '<div class="center-result"><div class="t-cell"><div class="win-bg"><h1>Правильно</h1> <br><div class="btnGreen" id="btnNext" onclick="nextQuest();" >Далее</div></div></div></div>';
+		summ += 5;
+	}
+	else{
+		winTitle.innerHTML = '<div class="center-result"><div class="t-cell"><div class="win-bg"><h1>Не правилно</h1> <br><div class="btnGreen" id="btnNext" onclick="nextQuest();" >Далее</div></div></div></div>';
+	}
+	var radioDis = document.getElementsByName("r");
+	for (var i = 0; i < radioDis.length;  i++){
+    radioDis[i].disabled = true;
+	}
+}
+/*function randomInteger() {
     var rand = 0;
     var flag = 0;
- while (randSumm.length<limit) {
+ 	while (randSumm.length<limit) {
     	rand = Math.floor((Math.random() * happyArray.length) +1);
     	for (var i = 0; i < randSumm.length; i++) {
     		if (randSumm[i] == rand) {
@@ -91,7 +91,6 @@ function randomInteger() {
     		randSumm.push(rand);
     	}
     	flag = 0;
-    	
     	console.log(randSumm.length, rand, i, randSumm.length, randSumm);
     	i++;
     }
@@ -99,3 +98,4 @@ function randomInteger() {
     return randSumm;
 }
 randomInteger();
+*/
